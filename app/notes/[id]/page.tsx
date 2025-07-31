a14b4ab2b57c4c3533354513b0621c25d1226538
Kamila <kamillkaa95@gmail.com>
2025-08-01 01:06:44 +0300

    hhggffdds

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,19 +1,20 @@
-// app/notes/[id]/page.tsx
+
 import { fetchNoteById } from "@/lib/api";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import NoteDetailsClient from "../NoteDetails.client";
 
 interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>; 
 }
 
 export default async function Page({ params }: PageProps) {
+  const { id } = await params; 
   const queryClient = new QueryClient();
 
   try {
     await queryClient.prefetchQuery({
-      queryKey: ["note", params.id],
-      queryFn: () => fetchNoteById(params.id),
+      queryKey: ["note", id],
+      queryFn: () => fetchNoteById(id),
     });
   } catch (error) {
     console.error("Failed to prefetch note:", error);
